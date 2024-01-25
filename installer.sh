4e3b339817978a4315f0fc62110ba4cd9d590fee
Alexander <153863591+AAlx0451@users.noreply.github.com>
2024-01-25 08:07:33 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 cd $HOME
 pkg update && pkg upgrade
 pkg install root-repo 
-pkg install -y git tsu python wpa-supplicant pixiewps iw
+pkg install git tsu python wpa-supplicant pixiewps iw -y
 
 termux-setup-storage
 
@@ -10,15 +10,32 @@ git clone --depth 1 https://github.com/AAlx0451/OneShot OneShot
 
 chmod +x OneShot/oneshot.py
 
-cd OneShot
-git pull
 cd $HOME
 echo alias oneshot='"cd $HOME/OneShot && sudo python oneshot.py -i wlan0 -K"' >> .bashrc
 cd /data/data/com.termux/files/usr/etc
 echo  >>motd
-echo To run OneShot: oneshot >>motd
+echo Utilities:>>motd
+echo WPS Hack (OneShot): oneshot>>motd
 
-printf "###############################################\n#  All done! Now you can run OneShot with\n#   sudo python OneShot/oneshot.py -i wlan0 -K\n#\n#  To update, run\n#   (cd OneShot && git pull)\n###############################################\n"
 
+echo Termux will restart, wait, type "oneshot" to start OneShot
+echo To run in force mode: "oneshot -F"
+echo
 echo Code by AAlx
-echo Join ЧВК Фембойчик
+sleep 5
+echo 15 seconds left
+sleep 5
+echo 10 seconds left
+sleep 5
+echo 5 seconds left
+sleep 1
+echo 4 seconds left
+sleep 1
+echo 3 seconds left
+sleep 1
+echo 2 seconds left
+sleep 1
+echo 1 second left
+sleep 1
+exit
+
