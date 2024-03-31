fdfb18c1e1b056ee8640d84fea1f53a7f90b9eff
Alexander <153863591+AAlx0451@users.noreply.github.com>
2024-03-31 21:48:31 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,19 +4,23 @@ pkg update && pkg upgrade
 pkg install root-repo 
 pkg install git tsu python wpa-supplicant pixiewps iw -y
 
-termux-setup-storage
-
 git clone --depth 1 https://github.com/AAlx0451/OneShot OneShot
 
 chmod +x OneShot/oneshot.py
 
 cd $HOME
-echo alias oneshot='"cd $HOME/OneShot && sudo python oneshot.py -i wlan0 -K"' >> .bashrc
-cd /data/data/com.termux/files/usr/etc
-echo  >>motd
-echo Utilities:>>motd
-echo OneShot WPS Hack: oneshot>>motd
+echo alias oneshot='"cd $HOME/OneShot && sudo python oneshot.py -i wlan0 -K -w"' >> .bashrc
 
+echo Do you want to edit "hello" message? y/n
+read $hello
+
+if [[ "$hello" == "y" ]]; then
+  cd /data/data/com.termux/files/usr/etc
+  echo  >>motd
+  echo Utilities:>>motd
+  echo OneShot WPS Hack: oneshot>>motd
+else
+  echo Ok, skip it
 
 echo Type "oneshot" to start OneShot
 echo To run in force mode: "oneshot -F"
