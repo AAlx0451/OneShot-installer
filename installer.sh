5224a66dbae48fef1de9571bbf89db327e4284e4
Alexander <153863591+AAlx0451@users.noreply.github.com>
2024-04-16 21:00:02 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -11,21 +11,21 @@ chmod +x OneShot/oneshot.py
 cd $HOME
 echo alias oneshot='"cd $HOME/OneShot && sudo python oneshot.py -i wlan0 -K -w"' >> .bashrc
 
-echo Do you want to edit "hello" message? y/n
+echo 'Do you want to edit "hello" (motd) message? y/n'
 read $hello
 
-if [[ "$hello" == "y" ]]; then
+if [[ $hello == "y" || $hello == "Y" ]]; then
   cd /data/data/com.termux/files/usr/etc
   echo  >>motd
   echo Utilities:>>motd
   echo OneShot WPS Hack: oneshot>>motd
-else
-  echo Ok, skip it
+  clear
+  cat /data/data/com.termux/files/usr/etc/motd && echo  
 fi
 
 echo Type "oneshot" to start OneShot
 echo To run in force mode: "oneshot -F"
+echo To run in bruteforce mode: "oneshot -B"
 echo
 echo Code by AAlx
 bash
-rm -rf $HOME/OneShot-installer
