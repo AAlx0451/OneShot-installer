4403bb1e6a670b228df66f31f6eeb6fc40505197
Alexander <153863591+AAlx0451@users.noreply.github.com>
2024-10-12 16:58:38 +0300

    Update installer.sh
    V10 update

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 cd $HOME
 pkg update && pkg upgrade
-pkg install root-repo 
+pkg install root-repo
 pkg install git tsu python wpa-supplicant pixiewps iw -y
 
 git clone --depth 1 https://github.com/AAlx0451/OneShot OneShot
@@ -11,18 +11,16 @@ chmod +x OneShot/oneshot.py
 cd $HOME
 echo alias oneshot='"cd $HOME/OneShot && sudo python oneshot.py -i wlan0 -K -w"' >> .bashrc
 
-echo 'Do you want to edit "hello" (motd) message? y/n'
+echo 'Do you want to edit "hello" (motd) message? It will add Utilities title to hello message y/n'
 read $hello
 
 if [[ $hello == "y" || $hello == "Y" ]]; then
-  cd /data/data/com.termux/files/usr/etc
-  echo  >>motd
-  echo Utilities:>>motd
-  echo OneShot WPS Hack: oneshot>>motd
-  clear
-  cat /data/data/com.termux/files/usr/etc/motd && echo  
+	echo  >>/data/data/com.termux/files/usr/etc/motd
+	echo Utilities:>>/data/data/com.termux/files/usr/etc/motd
+	echo OneShot WPS Hack: oneshot>>/data/data/com.termux/files/usr/etc/motd
 fi
 
+cat /data/data/com.termux/files/usr/etc/motd && echo
 echo Type "oneshot" to start OneShot
 echo To run in force mode: "oneshot -F"
 echo To run in bruteforce mode: "oneshot -B"
